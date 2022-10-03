77ac8645aa2b011bb0fcd3652dec2e386108f26c
Ashlee Bartley <80643645+ashleenikol@users.noreply.github.com>
2022-10-02 20:14:13 -0500

    Added form for Search Engine & HTML for weather

--- src/Weather.js
+++ src/Weather.js
@@ -2,9 +2,42 @@ import React from "react";
 
 export default function Weather() {
   return (
-    <div>
-      <h1>Testing Weather.js</h1>
-      <button className="btn btn-primary">Search</button>
+    <div className="container">
+      <div className="form">
+        <form className="search-engine">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Enter City"
+          />
+          <button>Search</button>
+        </form>
+      </div>
+      <div className="city-weather">
+        <h1 className="city">Tompkinsville</h1>
+        <h2 id="date-time">Thursday, 12:00PM</h2>
+        <div className="row">
+          <div className="col-6">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="sunny"
+              width="100"
+            />
+            Cloudy
+            <h2 id="temp"> 15°F</h2>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>
+                Humidity: <span id="humidity"></span>12%
+              </li>
+              <li>
+                Wind: <span id="wind"></span>5mph
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
