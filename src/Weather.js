c947aef06d6757535ba95ecf8361969d8837c82a
Ashlee Bartley <80643645+ashleenikol@users.noreply.github.com>
2022-10-02 19:52:59 -0500

    Created project & added Bootstrap

---
+++ src/Weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+    <div>
+      <h1>Testing Weather.js</h1>
+      <button className="btn btn-primary">Search</button>
+    </div>
+  );
+}
